09f80f496a295912f917fd2369deb00e1c4b37c3
FRANCISCO DE ASSIS PEREIRA SILVA <francisco.assis@a.ucb.br>
2026-08-27 21:16:04 -0300

    calcular divisao

--- 67.c
+++ 67.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main(void){
-    printf("Meu primeiro versionamento de code.");
+int main() {
+    int a = 5, b = 2;
+    int inteiro = a / b;          // Resultado: 2
+    int resto = a % b;            // Resultado: 1
+    float real = (float)a / b;    // Resultado: 2.5
+
+    printf("Divisao inteira: %d\n", inteiro);
+    printf("Resto: %d\n", resto);
+    printf("Divisao real: %.1f\n", real);
     return 0;
 }
